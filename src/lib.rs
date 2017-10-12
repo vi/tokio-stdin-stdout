698fb0c0fec29620dd30e8192b4a740219714890
Vitaly _Vi Shukela <vi0oss@gmail.com>
2017-10-13 00:51:53 +0300

    Not so good attempt

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,78 @@ const BUFSIZ: usize = 8192;
 use std::io::{Error, ErrorKind, Result, Read, Write};
 use futures::{Stream,Poll,Async,Sink,Future,AsyncSink};
 use tokio_io::{AsyncRead,AsyncWrite};
+use futures::sync::mpsc::{Receiver, Sender};
 
 type BBR = futures::sync::mpsc::Receiver <Box<[u8]>>;
 type BBS = futures::sync::mpsc::Sender   <Box<[u8]>>;
+
+use std::sync::Arc;
+use std::sync::{Mutex, Condvar};
+
+struct Traveller<BB> (
+    Arc<(Mutex<Option<BB>>,Condvar)>,
+);
+
+impl<BB> Traveller<BB> {
+    fn new(x:BB) -> Self {
+        Traveller(
+            Arc::new((Mutex::new(Some(x)),Condvar::new())),
+        )
+    }
+    fn off_we_go(&self) -> Option<BB> {
+        if let Ok(mut x) = (self.0).0.lock() {
+            x.take()
+        } else {
+            None
+        }
+    }
+    fn return_home(&self, b:BB, with_fanfares : bool) -> ::std::result::Result<(), BB> {
+        if let Ok(mut x) = (self.0).0.lock() {
+            if x.is_none() {
+                *x = Some(b);
+                if with_fanfares {
+                    (self.0).1.notify_one();
+                }
+                Ok(())
+            } else {
+                Err(b)
+            }
+        } else {
+            Err(b)
+        }
+    }
+    fn wait_for_homecoming_party<F:Fn(&BB)->bool> (&self, requirement:F) -> ::std::result::Result<BB,()> {
+        loop {
+            if let Ok(mut x) = (self.0).0.lock() {
+                if let Some(z) = x.take() {
+                    if requirement(&z) {
+                        return Ok(z)
+                    } else {
+                        *x = Some(z);
+                    }   
+                }
+                if let Ok(mut y) = (self.0).1.wait(x) {
+                    if let Some(z) = y.take() {
+                        if requirement(&z) {
+                            return Ok(z)
+                        } else {
+                            *y = Some(z);
+                        }
+                    }
+                } else {
+                    return Err(())
+                }
+            } else {
+                return Err(())
+            }
+        }
+    }
+}
+impl<BB> Clone for Traveller<BB> {
+    fn clone(&self) -> Self {
+        Traveller(self.0.clone())
+    }
+}
 
 pub struct ThreadedStdin {
     debt : Option<Box<[u8]>>,
@@ -76,28 +145,53 @@ impl Read for ThreadedStdin {
     }
 }
 
-
-
-pub struct ThreadedStdout {
-    snd : BBS,
+struct WriteOp {
+    buf : [u8; BUFSIZ],
+    len : isize,
+    task : Option<futures::task::Task>,
 }
+type WriteOpBox = Box<WriteOp>;
+
+pub struct ThreadedStdout (
+    Traveller<WriteOpBox>,
+);
 pub fn stdout() -> ThreadedStdout {
-    let (snd, rcv) : (BBS,BBR) =  futures::sync::mpsc::channel(0);
+    //let (snd, rcv) = futures::sync::mpsc::channel(0);
+    let t1 = Traveller::new(Box::new(WriteOp { 
+        buf: [0; BUFSIZ], 
+        len:0, 
+        task: None 
+    }));
+    let t2 = t1.clone();
     std::thread::spawn(move || {
         let sout = ::std::io::stdout();
         let mut sout_lock = sout.lock();
-        for b in rcv.wait() {
+        while let Ok(mut x) = t1.wait_for_homecoming_party(|x|x.len > 0) {
+            if let Err(_) = sout_lock.write_all(&x.buf[0..(x.len as usize)]) {
+                x.len = -1;
+            } else {
+                x.len = 0;
+            }
+            
+            let task = x.task.clone();
+            t1.return_home(x, false);
+            
+            if let Some(t) = task {
+                t.notify();
+            } else {
+                eprintln!("?");
+            }
+        }
+        /*for b in rcv.wait() {
             if let Err(_) = b {
                 break;
             }
             if let Err(_) = sout_lock.write_all(&b.unwrap()) {
                 break;
             }
-        }
+        }*/
     });
-    ThreadedStdout {
-        snd,
-    }
+    ThreadedStdout(t2)
 }
 impl AsyncWrite for ThreadedStdout {
     fn shutdown(&mut self) -> Poll<(), Error> {
@@ -107,7 +201,31 @@ impl AsyncWrite for ThreadedStdout {
 }
 impl Write for ThreadedStdout {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        match self.snd.start_send(buf.to_vec().into_boxed_slice()) {
+        let would_block1 : Error = ErrorKind::WouldBlock.into();
+        let would_block2 : Error = ErrorKind::WouldBlock.into();
+        
+        let mut x = self.0.off_we_go().ok_or(would_block1)?;
+        let l = x.len;
+        
+        
+        if l!=0 {
+            self.0.return_home(x, true);
+            if l < 0 { return Err(ErrorKind::Other.into()); }
+            return Err(would_block2);
+        }
+        
+        let l = std::cmp::min(x.buf.len(), buf.len());
+        x.buf[0..l].copy_from_slice(&buf[0..l]);
+        x.len = l as isize;
+        x.task = Some(futures::task::current());
+        
+        if let Err(_) = self.0.return_home(x, true) {
+            // Can't properly unwrap
+            panic!("Assertion failed");
+        }
+        
+        //self.buf.home_sweet_home
+        /*match self.snd.start_send(buf.to_vec().into_boxed_slice()) {
             Ok(AsyncSink::Ready)       => (),
             Ok(AsyncSink::NotReady(_)) => return Err(ErrorKind::WouldBlock.into()),
             Err(_)                     => return Err(ErrorKind::Other.into()),
@@ -117,8 +235,8 @@ impl Write for ThreadedStdout {
             Ok(Async::Ready(_))    => (), // OK
             Ok(Async::NotReady)    => (), // don't know what to do here
             Err(_) => return Err(ErrorKind::Other.into()),
-        }
-        Ok(buf.len())
+        }*/
+        Ok(l)
     }
     fn flush(&mut self) -> Result<()> {
         // XXX
