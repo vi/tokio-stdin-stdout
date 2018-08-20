dadc2ecb3d58be3bf33d5a509f95b40247e3147f
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-08-20 20:43:25 +0300

    Imrove the line-by-line example

--- examples/line-by-line.rs
+++ examples/line-by-line.rs
@@ -5,8 +5,7 @@ extern crate tokio_codec;
 
 use tokio::prelude::future::ok;
 use tokio::prelude::{Future, Stream};
-use tokio_io::io::write_all as tokio_write_all;
-use tokio_codec::{FramedRead, LinesCodec};
+use tokio_codec::{FramedRead, FramedWrite, LinesCodec};
 
 fn async_op(input: String) -> Box<Future<Item = String, Error = ()> + Send> {
   Box::new(ok(input.to_ascii_uppercase()))
@@ -17,18 +16,29 @@ fn main() {
   let stdout = tokio_stdin_stdout::stdout(0).make_sendable();
 
   let framed_stdin = FramedRead::new(stdin, LinesCodec::new());
-  let future = framed_stdin.for_each(move |line| {
-    let stdout_ = stdout.clone();
-    let future_compute = async_op(line).and_then(|mut result| {
-      result.push_str("\n");
-      let future_write = tokio_write_all(stdout_, result).map(drop).map_err(drop);
-      return future_write;
+  let framed_stdout = FramedWrite::new(stdout, LinesCodec::new());
+  
+  let future = framed_stdin
+    .and_then(move |line| {
+      // `and_then` above is not a Future's "and_then", it Stream's "and_then".
+      async_op(line)
+      .map_err(|err|panic!("Error: {:?}", err))
+    })
+    .forward(framed_stdout)
+    .map(|(_framed_stdin,_framed_stdout)|{
+      // _framed_stdin is exhaused now
+      // _framed_stdout is closed by `forward` above and is also unusable
+      
+      // You may try with `send_all` approach if you need other behaviour,
+      // but remember to `shutdown` the `stdout` to avoid existing before
+      // the data is actually delivered to stdout.
+      
+      // this `map` is needed to bring the final type to (), 
+      // as typically required for executing a future.
+    })
+    .map_err(|err| {
+      panic!("Error: {:?}", err);
     });
-
-    future_compute.map_err(|err|panic!("Error: {:?}",err))
-  }).map_err(|err| {
-    panic!("Error: {:?}", err);
-  });
 
   tokio::run(future);
 }
